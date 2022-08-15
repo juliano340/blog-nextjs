1094dd39ec91829b8edd96ec0480ede1c45c3ca1
juliano340 <juliano340@gmail.com>
2022-08-15 20:15:30 -0300

    Criação da página home estática.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,60 @@
-
-
+import Head from '../../node_modules/next/head';
+import style from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
-    <div>
-      Teste
-    </div>
+    <>
+      <Head>
+        <title>Apaixonado por tecnologia - Blog Sujeito Programador</title>
+      </Head>
+
+      <main className={style.container}>
+        <div className={style.containerHeader}>
+
+          <section className={style.ctaText}>
+            <h1>Levando você ao próximo nível!</h1>
+            <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Modi nam atque earum architecto, officiis magnam blanditiis! 
+              Labore officiis veniam enim repudiandae ea neque voluptate, 
+              eius, atque temporibus id ducimus.
+            </span>
+            <a>
+              <button>COMEÇAR AGORA!</button>
+            </a>
+
+          </section>
+
+          <img src="/images/banner-conteudos.png" alt="Conteúdos Sujeito Programador"/>
+        </div>
+
+        <hr className={style.divisor} />
+
+        <div className={style.sectionContent}>
+
+          <section>
+            <h2>Aprenda a criar aplicativos para Android e IOS</h2>
+            <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque a voluptatum consectetur aliquid perspiciatis voluptas animi doloribus incidunt fugit adipisci?</span>
+          </section>
+          <img src="/images/financasApp.png" alt="Conteúdos Mobile App" />
+
+        </div>
+
+        <hr className={style.divisor} />
+
+        <div className={style.sectionContent}>
+          
+          <img src="/images/webDev.png" alt="Conteúdos Mobile App" />
+
+          <section>
+
+            <h2>Aprenda a criar sistemas online!</h2>
+            <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, fugiat?</span>
+          </section>
+
+</div>
+
+
+      </main>
+    </>
   )
 }
